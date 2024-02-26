59f75dc05e8cafc8d22ff3e99dbc233f0057c71d
Frees3c <joeyjordanbarnes@hotmail.com>
2024-02-26 22:44:52 +0000

    tweaked some colors

--- lua/cyberdream/colors.lua
+++ lua/cyberdream/colors.lua
@@ -1,21 +1,32 @@
 local M = {}
 
 M.default = {
+    none = "NONE",
     bg = "#16181a",
     bgAlt = "#1e2124",
     bgHighlight = "#3c4048",
     fg = "#ffffff",
+    fg_dark = "#D0D0D0",
+    darkGrey1 = "#303030",
     darkGrey = "#232223",
     grey = "#7b8496",
+    lblue = "#1fa8dc",
     blue = "#5ea1ff",
     green = "#5eff6c",
+    dim_green = "#2F7F26",
     cyan = "#5ef1ff",
+    teal = "#008080",
     red = "#ff6e5e",
     yellow = "#f1ff5e",
     magenta = "#ff5ef1",
     pink = "#ff5ea0",
     orange = "#ffbd5e",
     purple = "#bd5eff",
+    gitsigns = {
+        add = "#41B235",
+        change = "#86f4ff",
+        delete = "#ff6e5e",
+    },
 }
 
 return M
